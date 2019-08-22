4cf8cd6ae3ca309a935e6272e1e661e3447b56db
Ivan Khavantsev <ivan.khavantsev@gmail.com>
2019-08-22 15:17:40 +0600

    Java timestamp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,21 @@
+import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cms.CMSAlgorithm;
+import org.bouncycastle.cms.jcajce.JcaSimpleSignerInfoVerifierBuilder;
+import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.tsp.TSPException;
 import org.bouncycastle.tsp.TimeStampRequest;
 import org.bouncycastle.tsp.TimeStampRequestGenerator;
 import org.bouncycastle.tsp.TimeStampResponse;
+import org.bouncycastle.util.Store;
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemWriter;
 
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.security.MessageDigest;
+import java.util.Collection;
+import java.util.Iterator;
 
 public class Main {
 
@@ -21,7 +27,19 @@ public class Main {
         byte[] document = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         TimestampResult result = makeTimestamp(document);
-        result.validate();
+        if(validateSign(result.getTsr(), "D:/timestamp/tsa.pem")){
+            System.out.println("CORRENT SIGN");
+        }else{
+            System.out.println("INCORRECT SIGN");
+        }
+
+        if(result.validate()){
+            System.out.println("TIMESTAMP VALID");
+        }else{
+            System.out.println("TIMESTAMP NOT VALID");
+        }
+
+
         System.out.println(result.getTsr().getTimeStampToken().getTimeStampInfo().getGenTime());
 
     }
@@ -46,8 +64,13 @@ public class Main {
             this.tsr = tsr;
         }
 
-        public void validate() throws TSPException {
-            tsr.validate(tsq);
+        public boolean validate() throws TSPException {
+            try {
+                tsr.validate(tsq);
+                return true;
+            } catch (TSPException e) {
+                return false;
+            }
         }
     }
 
@@ -66,10 +89,35 @@ public class Main {
             result.setTsr(tsr);
             return result;
         } catch (Throwable t) {
-            throw new Exception("Невозможно получить timestamp");
+            throw new Exception("Невозможно получить timestamp", t);
         }
     }
 
+    public boolean validateSign(TimeStampResponse tsr, String certFileName) throws Throwable{
+        X509CertificateHolder cert2 = getCertificateFromFile(certFileName);
+        return tsr.getTimeStampToken().isSignatureValid(new JcaSimpleSignerInfoVerifierBuilder().build(cert2));
+    }
+
+    public X509CertificateHolder getCertificateFromFile(String filename) throws Throwable{
+        FileReader fileReader = new FileReader(new File(filename));
+        PEMParser pemParser = new PEMParser(fileReader);
+        X509CertificateHolder certificateHolder = (X509CertificateHolder) pemParser.readObject();
+        return certificateHolder;
+    }
+
+    public void writeX509ToFile(X509CertificateHolder cert) throws Throwable{
+        PemWriter writer = new PemWriter(new FileWriter(new File("tsa.pem")));
+        writer.writeObject(new PemObject("CERTIFICATE", cert.toASN1Structure().getEncoded()));
+        writer.flush();
+    }
+
+    public X509CertificateHolder getCertFromTsr(TimeStampResponse tsr) {
+        Store storeTt = tsr.getTimeStampToken().getCertificates();
+        Collection collTt = storeTt.getMatches(tsr.getTimeStampToken().getSID());
+        Iterator certIt2 = collTt.iterator();
+        X509CertificateHolder cert = (X509CertificateHolder)certIt2.next();
+        return  cert;
+    }
 
     private TimeStampResponse getServiceTimestamp(byte[] tsqData) throws Throwable {
 
